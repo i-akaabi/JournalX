0505cad95c5527486a8f329e202e12302b364a8e
Amir <a.mazhary1999@gmail.com>
2026-05-18 15:24:44 +0200

    Registrierungsseite und neue Flask Routen für erweiterten Benutzerfluss in JournalX ergänzt

--- app.py
+++ app.py
@@ -88,6 +88,13 @@ def login_page():
 
     return render_template("login.html")
 
+# Registrierungsseite anzeigen
+
+@app.route("/register-page")
+def register_page():
+
+    return render_template("register.html")
+
 
 # ---------------------------------------------------
 # ALLE EINTRÄGE ANZEIGEN (READ)
